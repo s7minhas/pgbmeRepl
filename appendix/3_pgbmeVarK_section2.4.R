c2f89fac2064f88d861a8d876d6ab6da8dd56c4c
s7minhas <s7.minhas@gmail.com>
2018-06-11 00:29:01 -0400

    finalize var by k results

--- appendix/3_pgbmeVarK_section2.4.R
+++ appendix/3_pgbmeVarK_section2.4.R
@@ -44,10 +44,10 @@ rm(xData)
 ################################
 
 # run pgbme varying k ###############################
-if(!all(paste0('results2012_k', 1:5, '.rda') %in% list.files())){
-	cores = 5
+if(!all(paste0('results2012_k', 1:3, '.rda') %in% list.files())){
+	cores = 3
 	cl=makeCluster(cores) ; registerDoParallel(cl)
-	shhh <- foreach(rank = 1:5, 
+	shhh <- foreach(rank = 1:3, 
 		.packages=c('lme4','magic','msm','mnormt')
 		) %dopar% {
 		set.seed(6886)
@@ -64,7 +64,7 @@ if(!all(paste0('results2012_k', 1:5, '.rda') %in% list.files())){
 ################################
 
 # pull out yhats ###############################
-predDF <- lapply(1:5, function(rank){
+predDF <- lapply(1:3, function(rank){
 	load(paste0('results2012_k',rank,'.rda'))
 	yhat <- calc_yhat(est, FALSE); rm(est)
 	yhat <- apply(pnorm(yhat), 1, mean)
@@ -72,7 +72,7 @@ predDF <- lapply(1:5, function(rank){
 	diag(yhat) = NA ; pgbmeProb = c(yhat) ; rm(yhat)
 	return(pgbmeProb) })
 predDF <- data.frame(do.call('cbind', predDF))
-names(predDF) = paste0('k_',1:5)
+names(predDF) = paste0('k_',1:3)
 ################################
 
 # calc auc stats ###############################
@@ -80,13 +80,13 @@ predDF$y = c(data.matrix(bit.acc.t[[ '2012' ]]))
 predDF = na.omit(predDF)
 
 # run perf checks
-perfStats = lapply(names(predDF)[1:5], function(k){
+perfStats = lapply(names(predDF)[1:3], function(k){
 	aucROC=roc.curve(predDF[predDF$y==1,k], predDF[predDF$y==0,k])$auc
 	aucPR=pr.curve(predDF[predDF$y==1,k], predDF[predDF$y==0,k])$auc.integral
 	return( c('AUC (ROC)'=aucROC,'AUC (PR)'=aucPR) )
 })
 perfStats = do.call('rbind', perfStats)
-rownames(perfStats) = names(predDF)[1:5]
+rownames(perfStats) = names(predDF)[1:3]
 sink(file='tableA3.txt')
 print(round(perfStats, 2))
 sink()
